e84384f5e180f0fcf5c848c81f47c954bc0d4efd
Tiago Resende <tiagor87@gmail.com>
2015-10-28 16:11:23 -0200

    Implementa documentação do builder de frete.

--- builders/shipping_builder.ts
+++ builders/shipping_builder.ts
@@ -4,53 +4,132 @@ import {IAddress} from "../models/address_model";
 import {IShipping} from "../models/shipping_model";
 import {Shipping} from "../models/shipping_model";
 import {EnumShipping} from "../models/shipping_model";
-import {IPaymentBuilder} from "./pagseguro_builder";
+import {IPaymentBuilder} from "./payment_builder";
 import {IAddressBuilder} from "./address_builder";
 import {AddressBuilder} from "./address_builder";
 
+/**
+ * @interface
+ * @description Builder de frete.
+ */
 export interface IShippingBuilder {
-    ofType(type:EnumShipping):IShippingBuilder;
-    withAddress(address?: IAddress):IAddressBuilder;
-    andCost(cost:number):IShippingBuilder;
-    build():IShipping;
-    buildAndReturn():IPaymentBuilder;
-    return():IPaymentBuilder;
+    /**
+     * @method
+     * @param {EnumShipping} type Tipo do frete.
+     * @returns {IShippingBuilder}
+     * @description Preenche o tipo do frete.
+     */
+    ofType(type: EnumShipping): IShippingBuilder;
+    /**
+     * @method
+     * @param {IAddress} address Endereço de entrega.
+     * @returns {IAddressBuilder}
+     * @description Preenche o endereço de entrega.
+     */
+    withAddress(address?: IAddress): IAddressBuilder;
+    /**
+     * @method
+     * @param {number} cost Custo do frete.
+     * @returns {IShippingBuilder}
+     * @description Preenche o custo do frete.
+     */
+    andCost(cost: number): IShippingBuilder;
+    /**
+     * @method
+     * @returns {IShipping}
+     * @description Constrói o frete com os dados fornecidos.
+     */
+    build(): IShipping;
+    /**
+     * @method
+     * @returns {IPaymentBuilder}
+     * @description Contrói o frete com os dados fornecidos e retorna ao builder de pagamento.
+     */
+    buildAndReturn(): IPaymentBuilder;
+    /**
+     * @method
+     * @returns {IPaymentBuilder}
+     * @description Retorna ao builder de pagamento.
+     */
+    return(): IPaymentBuilder;
 }
 
+/**
+ * @class
+ * @description Builder de frete.
+ */
 export class ShippingBuilder implements IShippingBuilder {
     private shipping: IShipping;
+
+    /**
+     * @constructor
+     * @param {IPaymentBuilder} builder
+     */
     constructor(private builder: IPaymentBuilder) {
         this.shipping = new Shipping();
     }
 
-    ofType(type:EnumShipping):IShippingBuilder {
+    /**
+     * @method
+     * @param {EnumShipping} type Tipo do frete.
+     * @returns {IShippingBuilder}
+     * @description Preenche o tipo do frete.
+     */
+    ofType(type: EnumShipping): IShippingBuilder {
         this.shipping.type = type;
         return this;
     }
 
-    withAddress(address?: IAddress):IAddressBuilder {
+    /**
+     * @method
+     * @param {IAddress} address Endereço de entrega.
+     * @returns {IAddressBuilder}
+     * @description Preenche o endereço de entrega.
+     */
+    withAddress(address?: IAddress): IAddressBuilder {
         if (address) {
             this.shipping.address = address;
         }
         return new AddressBuilder(this);
     }
 
-    andCost(cost: number):IShippingBuilder {
+    /**
+     * @method
+     * @param {number} cost Custo do frete.
+     * @returns {IShippingBuilder}
+     * @description Preenche o custo do frete.
+     */
+    andCost(cost: number): IShippingBuilder {
         this.shipping.cost = cost;
         return this;
     }
 
+    /**
+     * @method
+     * @returns {IShipping}
+     * @description Constrói o frete com os dados fornecidos.
+     */
     build(): IShipping {
         return this.shipping;
     }
 
-    buildAndReturn():IPaymentBuilder {
+    /**
+     * @method
+     * @returns {IPaymentBuilder}
+     * @description Contrói o frete com os dados fornecidos e retorna ao builder de pagamento.
+     */
+    buildAndReturn(): IPaymentBuilder {
         this.shipping.save();
         this.builder.withShipping(this.shipping);
         return this.builder;
     }
 
-    return():IPaymentBuilder {
+    /**
+     * @method
+     * @returns {IPaymentBuilder}
+     * @description Retorna ao builder de pagamento.
+     */
+    return(): IPaymentBuilder {
         return this.builder;
     }
 }
