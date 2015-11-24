44c5fe419e124b4666fa2ddc9f6495d136e6a039
Tiago Resende <tiagor87@gmail.com>
2015-11-24 18:32:28 -0200

    Ajusta status.

--- public/scripts/filters/transaction_status_filter.ts
+++ public/scripts/filters/transaction_status_filter.ts
@@ -8,6 +8,7 @@ angular.module("n4_payment")
         case 3: return "Pago";
         case 4: return "Disponível";
         case 5: return "Em disputa";
+        case 6: return "Devolvido";
         case 7: return "Cancelado";
         case 8: return "Valor devolvido";
         case 9: return "Em contestação";
